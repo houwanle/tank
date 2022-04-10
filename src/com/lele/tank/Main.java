571ef49aaa71c7a3f54956f63d4612966e88cf90
houwanle <wanle@qq.com>
2022-04-10 18:48:51 +0800

    去掉敌人坦克的初始化部分，为网络版做准备。

--- src/com/lele/tank/Main.java
+++ src/com/lele/tank/Main.java
@@ -11,12 +11,12 @@ public class Main {
         // 窗口
         TankFrame tf = new TankFrame();
 
-        int initTankCount = Integer.parseInt(PropertyMgr.get("initTankCount").toString());
+        /*int initTankCount = Integer.parseInt(PropertyMgr.get("initTankCount").toString());
 
         // 初始化敌方坦克
         for (int i = 0; i < initTankCount; i++) {
             tf.tanks.add(new Tank(50 + i*80, 200, Dir.DOWN, Group.BAD, tf));
-        }
+        }*/
 
         new Thread(() -> new Audio("audio/war1.wav").loop()).start();
 
